acbce4697f7ebcf5fd4830f9bc472cb336034fac
RAGHURAM GUDEMARANAHALLI NATARAJA <raghuram@pdx.edu>
2025-03-25 20:36:23 -0700

    add home page for git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import path, { dirname } from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "/PersonalPortfolio/", 
   plugins: [
     react(),
     runtimeErrorOverlay(),
